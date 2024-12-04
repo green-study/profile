19898e00e7a5c53a7051dbaf331f69a8433ca422
green-study <soo010410@naver.com>
2024-12-04 21:02:10 +0900

    added default design

---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="Title-margin-1" />
+            <div className="Title">
+                <p># Front-end Developer</p>
+            </div>
+            <hr />
+            <div className="Title-margin-2" />
+            <div className="Name">
+                <p>Subin's Portfolio</p>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
